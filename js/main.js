70dd09216643c9661004b92f4dbeba09ff2511bd
AlexChihaia <alexchihaia84@gmail.com>
2023-11-07 20:29:54 +0100

    feat : event listener class remover

--- js/main.js
+++ js/main.js
@@ -1,63 +1,29 @@
 'use strict';
 
+const passengerName = document.getElementById('passengerNameInput');
+const tripLenght = document.getElementById('kmNumberInput');
+const passengerAgeRange = document.getElementById('ageRangeInput');
+const show = document.getElementById('show');
 
 
-const priceElement = document.getElementById('price')
+const submitButton = document.getElementById('generate');
+const resetButton = document.getElementById('cancel');
 
-//1) Scrivere un programma che chieda all’utente:
+submitButton.addEventListener('click',
+    function () {
+        show.classList.remove('visually-hidden')
 
-//- Prompt con il numero di chilometri da percorrere 
-
-const tripLenght = Number(prompt('Inserisci km da percorrere'))
-
-
-//- Prompt con l'età del passeggero
-const passengerAge = Number(prompt('Inserisci la tua età'))
-
-//2) Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-//- definire il prezzo del biglietto in base ai km con un(0.21 € al km)
-//- chilometri da percorrere * prezzo(0.21 €)
-
-const ticketPrice = (tripLenght * 0.21)
-
-const kidsDiscount = 20
-const elderDiscount = 40
+    }
+)
 
 
+resetButton.addEventListener('click',
+    function () {
 
-if (!isNaN(tripLenght) && !isNaN(passengerAge)) {
-    console.log('km :' + 'sono un numero :' + tripLenght);
-}
-else {
-    console.log('km :' + 'non sono un numero :' + tripLenght);
-}
+    }
+)
 
 
 
 
 
-//3) - va applicato uno sconto del 20 % per i minorenni
-//   - va applicato uno sconto del 40 % per gli over 65.
-
-const kidsPrice = (ticketPrice - (ticketPrice / 100 * kidsDiscount));
-
-const kidsRounded = Math.round((kidsPrice + Number.EPSILON) * 100) / 100;
-
-const elderPrice = (ticketPrice - (ticketPrice / 100 * elderDiscount));
-
-const elderRounded = Math.round((elderPrice + Number.EPSILON) * 100) / 100;
-
-
-
-if (passengerAge < 18) {
-    console.log('Il prezzo del biglietto è :' + '' + kidsRounded + '' + '€');
-    priceElement.innerHTML = ('Il prezzo del biglietto è :' + '' + kidsRounded + '' + '€');
-}
-else if (passengerAge > 64) {
-    console.log('Il prezzo del biglietto è :' + '' + elderRounded + '' + '€');
-    priceElement.innerHTML = ('Il prezzo del biglietto è :' + '' + elderRounded + '' + '€');
-}
-else {
-    console.log('Il prezzo del biglietto è :' + '' + ticketPrice + '' + '€');
-    priceElement.innerHTML = ('Il prezzo del biglietto è :' + '' + ticketPrice + '' + '€');
-}
