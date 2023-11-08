1c1b7460469507fd7c848f4c3450c82799728610
AlexChihaia <alexchihaia84@gmail.com>
2023-11-08 16:02:59 +0100

    feat : correzioni

--- js/main.js
+++ js/main.js
@@ -1,21 +1,80 @@
 'use strict';
 
+
+/* dati form */
+
 const passengerName = document.getElementById('passengerNameInput');
 const tripLenght = document.getElementById('kmNumberInput');
 const passengerAgeRange = document.getElementById('ageRangeInput');
-const show = document.getElementById('show');
 
+
+
+/* dati biglietto virtuale */
+const nameDetail = document.getElementById('passengerName');
+let type = document.getElementById('type');
+const cabinNumber = document.getElementById('cabin');
+const cpCode = document.getElementById('cp');
+const finalPrice = document.getElementById('price');
+
+const randomNumberCabin = Math.floor(Math.random() * 12 + 1);
+const randomNumberCp = Math.floor(Math.random() * 9000) + 1000;
+
+
+/* variabile di display */
+const show = document.querySelector('div.d-none');
+
+
+
+/* button */
 
 const submitButton = document.getElementById('generate');
 const resetButton = document.getElementById('cancel');
 
+
+
+
+/* prezzo e sconti */
+
+const costoBigliettoKm = 0.21;
+const scontoMinorennePercentuale = 20;
+const scontoOverPercentuale = 40;
+
+
+
+/* calcolo del biglietto */
+
 submitButton.addEventListener('click',
     function () {
-        show.classList.remove('visually-hidden')
+        let ticketPrice = costoBigliettoKm * Number(tripLenght.value);
+        console.log(ticketPrice);
+
+
+
+        if (passengerAgeRange.value === 'minorenne') {
+            ticketPrice = ticketPrice - (ticketPrice * scontoMinorennePercentuale) / 100;
+            type.innerText = 'Sconto 20%';
+
+        }
+        else if (passengerAgeRange.value === 'over') {
+            ticketPrice = ticketPrice - (ticketPrice * scontoOverPercentuale) / 100;
+            type.innerText = 'Sconto 40%';
+        }
+        else {
+            type.innerText = 'Biglietto standard';
+        }
+        console.log(ticketPrice);
+        show.classList.remove('d-none');
+        nameDetail.innerText = passengerName.value;
+        cabinNumber.innerText = randomNumberCabin;
+        cpCode.innerText = randomNumberCp;
+        finalPrice.innerText = ticketPrice.toFixed(2) + ' ' + ('€');
 
     }
-)
+);
 
+
+
+/* reset del biglietto */
 
 resetButton.addEventListener('click',
     function () {
